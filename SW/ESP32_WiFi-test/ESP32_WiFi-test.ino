cdaef4ebda12e73b2a79e709fe5e9b64a586dbe4
Konstantin Láska <konstantin.laska@icloud.com>
2026-03-06 14:31:49 +0100

    Create ESP32_WiFi-test.ino

---
+++ SW/ESP32_WiFi-test/ESP32_WiFi-test.ino
@@ -0,0 +1,84 @@
+/*
+* Test code for LaskaKit ESP32-DEVKit
+* Test WiFi connection with WiFi Manager
+*
+* Board:    LaskaKit ESP32-DEVKit     https://www.laskakit.cz/laskakit-esp32-devkit/
+*
+* Library:  https://github.com/tzapu/WiFiManager
+*
+* 
+* Made by laskakit.cz (c) 2026
+*/
+ 
+#include <WiFi.h>
+#include <WiFiManager.h>   // 
+#include <HTTPClient.h>
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println();
+  Serial.println("=== ESP32 WiFi test ===");
+
+  WiFi.mode(WIFI_STA);
+
+  WiFiManager wm;
+
+  // Volitelně: timeout pro konfigurační portál
+  wm.setConfigPortalTimeout(180);
+
+  // Pokus o připojení, pokud nejsou uložené údaje, otevře AP
+  // Název AP: ESP32-WIFI-TEST
+  bool res = wm.autoConnect("ESP32-WIFI-TEST");
+
+  if (!res) {
+    Serial.println("Nepodarilo se pripojit k WiFi.");
+    Serial.println("Restartuji za 5 sekund...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  Serial.println("WiFi pripojena.");
+  Serial.print("IP adresa: ");
+  Serial.println(WiFi.localIP());
+
+  // Test stazeni stranky
+  HTTPClient http;
+
+  Serial.println("Stahuji https://www.laskakit.cz/ ...");
+
+  http.begin("https://www.laskakit.cz/");
+  int httpCode = http.GET();
+
+  if (httpCode > 0) {
+    Serial.print("HTTP kod: ");
+    Serial.println(httpCode);
+
+    String payload = http.getString();
+
+    Serial.println("=== Zacatek odpovedi ===");
+    if (payload.length() > 500) {
+      Serial.println(payload.substring(0, 500));
+    } else {
+      Serial.println(payload);
+    }
+    Serial.println("=== Konec ukazky odpovedi ===");
+  } else {
+    Serial.print("Chyba HTTP pozadavku: ");
+    Serial.println(http.errorToString(httpCode));
+  }
+
+  http.end();
+
+  // Odpojeni od WiFi
+  Serial.println("Odpojuji WiFi...");
+  WiFi.disconnect(true, true);
+  WiFi.mode(WIFI_OFF);
+
+  Serial.println("WiFi odpojena.");
+}
+
+void loop() {
+  // Nic dalsiho neni potreba
+}
